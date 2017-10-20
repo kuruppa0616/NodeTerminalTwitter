aadc93cb9a859a68c213c7aa0b5920574a5e40f6
くるっぱ <kuruppa0616@gmail.com>
2017-10-20 11:52:53 +0900

    tweetPrinter分離

---
+++ TweetPrinter.js
@@ -0,0 +1,188 @@
+/* eslint-disable no-console,indent */
+
+const Term = require("terminal-kit").terminal;
+const ImageToAscii = require("image-to-ascii");
+
+const TweetCacheController = require("./TweetCacheController");
+const TweetIdListController = require("./TweetIdListController");
+
+module.exports = class TweetPrinter {
+	constructor() {}
+
+	printTweet(tweet) {
+
+		tweet = this.printRetweet(tweet);
+		this.printUserName(tweet);
+		this.printBody(tweet);
+		this.printImage(tweet);
+		this.printQuoted(tweet);
+		this.drawBorderLine();
+		this.newline();
+
+	}
+	printRetweet(tweet) {
+		//リツートだったときは、名前を表示して元ツイートの詳細を表示
+		if (tweet.retweeted_status) {
+			Term.dim("RT:" + tweet.user.name + " Retweeted");
+			this.newline();
+			tweet = tweet.retweeted_status;
+		}
+		return tweet;
+	}
+
+	//引用リツイート元を表示させる
+	printQuoted(tweet) {
+		//引用リツイートだった場合元ツイート表示
+		if (tweet.is_quote_status) {
+			tweet = tweet.quoted_status;
+			Term("Quoted>>");
+			this.newline();
+			this.printUserName(tweet);
+			this.printBody(tweet);
+			this.printImage(tweet);
+		}
+		return tweet;
+	}
+
+	//Tweet本文を表示する
+	printBody(tweet) {
+		let syntax_list = [];
+		let text = tweet.text;
+
+		// 本文から画像のURLを消す
+		if (this.isMedia(tweet)) {
+			for (let medium of tweet.extended_entities.media) {
+				if (medium.type == "photo") {
+					text = text.replace(medium.url, "");
+
+				}
+			}
+		}
+
+		//url取り出し
+		//短縮URLを元URLと置換
+		for (let url of tweet.entities.urls) {
+			syntax_list.push(url.display_url.replace("?", "\\?"));
+			text = text.replace(url.url, url.display_url);
+		}
+
+		//ハッシュタグ取り出し
+		for (let hashtag of tweet.entities.hashtags) {
+			syntax_list.push("#" + hashtag.text);
+		}
+
+		// メンション取り出し
+		for (let user_mention of tweet.entities.user_mentions) {
+			syntax_list.push("@" + user_mention.screen_name);
+		}
+
+		//highlightする文字がある場合
+		if (syntax_list.length != 0) {
+			let reg = new RegExp("(" + syntax_list.join("|") + ")", "g");
+			let textList = text.split(reg);
+			for (let str of textList) {
+				if (str.search(reg) != -1) {
+					Term.blue(str);
+				} else {
+					Term(str);
+				}
+			}
+
+			//ない場合は全部一気に表示
+		} else {
+			Term(text);
+		}
+
+		this.newline();
+		return tweet;
+	}
+
+	//ユーザーネーム表示
+	printUserName(tweet) {
+		Term.bold(tweet.user.name);
+
+		//公式アカウント判定
+		if (tweet.user.verified) {
+			Term(" ✔ ");
+
+		}
+
+		// 鍵アカウント判定
+		if (tweet.user.protected) {
+			Term(" 鍵 ");
+		}
+
+		//スクリーンネーム表示
+		Term.dim(" @" + tweet.user.screen_name);
+
+		//時刻表示
+		Term.dim("\t" + this.toLocaleString(new Date(tweet.created_at)));
+
+		Term.dim("\t No:" + TweetIdListController.addID(tweet.id_str));
+		this.newline();
+		return tweet;
+	}
+
+	//画像表示
+	printImage(tweet) {
+		//Tweetに画像が含まれるかの判定
+		if (this.isMedia(tweet)) {
+			//添付されてるメディア要素を全て取り出し
+			for (let medium of tweet.extended_entities.media) {
+				//画像ならアスキー変換
+				if (medium.type == "photo") {
+					//アスキー変換のプロミス関数を登録
+					this.printAscii(medium);
+				}
+			}
+		}
+		return tweet;
+	}
+
+	//Tweetにメディアが含まれているかの判定
+	isMedia(tweet) {
+		return tweet.extended_entities && tweet.extended_entities.media;
+	}
+
+	printAscii(medium) {
+		ImageToAscii(medium.media_url + ":thumb", {
+			size: {
+				width: 30
+			}
+		}, (err, converted) => {
+			Term(err || converted);
+			this.newline();
+			Term(medium.media_url);
+			this.newline();
+			this.drawBorderLine();
+
+		});
+	}
+	//画面幅一杯のラインを引く
+	drawBorderLine() {
+		Term.dim("―".repeat(Term.width));
+		this.newline();
+	}
+
+	//改行
+	newline() {
+		console.log();
+	}
+
+	toLocaleString(date) {
+		return [
+				date.getFullYear(),
+				date.getMonth() + 1,
+				date.getDate()
+			].join("/") + " " +
+			date.toLocaleTimeString();
+	}
+	releaseCache() {
+		TweetCacheController.setIsTweetCache(false);
+		for (let temp of TweetCacheController.getTweetCacheList()) {
+			this.printTweet(temp);
+		}
+		TweetCacheController.clearTweetCacheList();
+	}
+
+};
--- main.js
+++ main.js
@@ -3,11 +3,13 @@
 console.log("Loading module...");
 
 const Term = require("terminal-kit").terminal;
-const ImageToAscii = require("image-to-ascii");
 
 const Auth = require("./Auth");
 const TweetCacheController = require("./TweetCacheController");
 const TweetIdListController = require("./TweetIdListController");
+const TweetPrinter = require("./TweetPrinter");
+const tweetPrinter = new TweetPrinter();
+
 
 console.log("Login twitter...");
 
@@ -63,7 +65,7 @@ Term.on("key", (name) => {
 					newline();
 					Term("Cancelled");
 					newline();
-					releaseCache();
+					tweetPrinter.releaseCache();
 					break;
 			}
 		});
@@ -94,7 +96,7 @@ function inputTweet(funcList, message) {
 			Term("Cancelled");
 			newline();
 		}
-		releaseCache();
+		tweetPrinter.releaseCache();
 	});
 }
 
@@ -144,16 +146,6 @@ function favTweet(tweetID) {
 	});
 }
 
-
-function releaseCache() {
-	TweetCacheController.setIsTweetCache(false);
-	for (let temp of TweetCacheController.getTweetCacheList()) {
-		printTweet(temp);
-	}
-	TweetCacheController.clearTweetCacheList();
-}
-
-
 //main処理ここまで
 
 //ストリーミング処理
@@ -164,181 +156,12 @@ function startStream() {
 		if (TweetCacheController.getIsTweetCache()) {
 			TweetCacheController.addCache(tweet);
 		} else {
-			printTweet(tweet);
+			tweetPrinter.printTweet(tweet);
 		}
 	});
 	stream.on("error", (e) => {
 		console.log(e);
 	});
-}
-
-//Tweet表示
-//Templateメソッドだわこれ
-function printTweet(tweet) {
-
-	tweet = printRetweet(tweet);
-	printUserName(tweet);
-	printBody(tweet);
-	printImage(tweet);
-	printQuoted(tweet);
-	drawBorderLine();
-	newline();
-
-}
-
-function printRetweet(tweet) {
-	//リツートだったときは、名前を表示して元ツイートの詳細を表示
-	if (tweet.retweeted_status) {
-		Term.dim("RT:" + tweet.user.name + " Retweeted");
-		newline();
-		tweet = tweet.retweeted_status;
-	}
-	return tweet;
-}
-
-//引用リツイート元を表示させる
-function printQuoted(tweet) {
-	//引用リツイートだった場合元ツイート表示
-	if (tweet.is_quote_status) {
-		tweet = tweet.quoted_status;
-		Term("Quoted>>");
-		newline();
-		printUserName(tweet);
-		printBody(tweet);
-		printImage(tweet);
-	}
-	return tweet;
-}
-
-//Tweet本文を表示する
-function printBody(tweet) {
-	let syntax_list = [];
-	let text = tweet.text;
-
-	// 本文から画像のURLを消す
-	if (isMedia(tweet)) {
-		for (let medium of tweet.extended_entities.media) {
-			if (medium.type == "photo") {
-				text = text.replace(medium.url, "");
-
-			}
-		}
-	}
-
-	//url取り出し
-	//短縮URLを元URLと置換
-	for (let url of tweet.entities.urls) {
-		syntax_list.push(url.display_url.replace("?", "\\?"));
-		text = text.replace(url.url, url.display_url);
-	}
-
-	//ハッシュタグ取り出し
-	for (let hashtag of tweet.entities.hashtags) {
-		syntax_list.push("#" + hashtag.text);
-	}
-
-	// メンション取り出し
-	for (let user_mention of tweet.entities.user_mentions) {
-		syntax_list.push("@" + user_mention.screen_name);
-	}
-
-	//highlightする文字がある場合
-	if (syntax_list.length != 0) {
-		let reg = new RegExp("(" + syntax_list.join("|") + ")", "g");
-		let textList = text.split(reg);
-		for (let str of textList) {
-			if (str.search(reg) != -1) {
-				Term.blue(str);
-			} else {
-				Term(str);
-			}
-		}
-
-		//ない場合は全部一気に表示
-	} else {
-		Term(text);
-	}
-
-	newline();
-	return tweet;
-}
-
-//ユーザーネーム表示
-function printUserName(tweet) {
-	Term.bold(tweet.user.name);
-
-	//公式アカウント判定
-	if (tweet.user.verified) {
-		Term.green(" ✔ ");
-
-	}
-
-	// 鍵アカウント判定
-	if (tweet.user.protected) {
-		Term(" 鍵 ");
-	}
-
-	//スクリーンネーム表示
-	Term.dim(" @" + tweet.user.screen_name);
-
-	//時刻表示
-	Term.dim("\t" + toLocaleString(new Date(tweet.created_at)));
-
-	Term.dim("\t No:" + TweetIdListController.addID(tweet.id_str));
-	newline();
-	return tweet;
-}
-
-//画像表示
-function printImage(tweet) {
-	//Tweetに画像が含まれるかの判定
-	if (isMedia(tweet)) {
-		//添付されてるメディア要素を全て取り出し
-		for (let medium of tweet.extended_entities.media) {
-			//画像ならアスキー変換
-			if (medium.type == "photo") {
-				//アスキー変換のプロミス関数を登録
-				printAscii(medium);
-			}
-		}
-	}
-	return tweet;
-}
-
-//Tweetにメディアが含まれているかの判定
-function isMedia(tweet) {
-	return tweet.extended_entities && tweet.extended_entities.media;
-}
-
-function printAscii(medium) {
-	ImageToAscii(medium.media_url + ":thumb", {
-		size: {
-			width: 30
-		}
-	}, (err, converted) => {
-		Term(err || converted);
-		newline();
-		Term(medium.media_url);
-		newline();
-		drawBorderLine();
-
-	});
-}
-
-//日付フォーマット変換
-function toLocaleString(date) {
-	return [
-			date.getFullYear(),
-			date.getMonth() + 1,
-			date.getDate()
-		].join("/") + " " +
-		date.toLocaleTimeString();
-}
-
-//画面幅一杯のラインを引く
-function drawBorderLine() {
-	Term.dim("―".repeat(Term.width));
-	newline();
 }
 
 //改行
